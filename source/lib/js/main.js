cb4eec7be031a2af68938be56e593cdaec95e002
Juan Costa <juandev1998@gmail.com>
2024-04-14 20:27:29 -0300

    decimo setimo commit

--- source/lib/js/main.js
+++ source/lib/js/main.js
@@ -1,1 +1,58 @@
+// mascaras de jQuery
+
+$('#telefone').mask('(00) 00000-0000');
+
+$(document).ready(function () {
+    $('#email').mask('A', {
+        translation: {
+            'A': { pattern: /[\w@\-.+]/, recursive: true }
+        }
+    });
+});
+
+$('form').validate({
+    rules:
+    {
+        nome: {
+            required: true
+        },
+        email: {
+            required: true,
+            email: true
+        },
+        telefone: {
+            required: true
+        },
+        mensagem: {
+            required: true
+        },
+        curriculo: {
+            required: true
+        }
+
+
+    },
+    messages: {
+        nome: 'Por favor, insira o seu nome',
+        email: 'Por favor, insira seu e-mail',
+        telefone: 'Por favor, insira seu telefone',
+        curriculo: 'Por favor, envie um documento PDF'
+
+    },
+    invalidHandler: function (evento, validador) {
+        let camposIncorretos = validador.numberOfInvalids()
+        if (camposIncorretos) {
+            alert(`Existem ${camposIncorretos} campos incorretos.`)
+        }
+    }
+})
+
+document.getElementById('trabalhe-conosco').addEventListener('submit', function (event){
+    var arquivoInput = document.getElementById('curriculo');
+    var arquivo = arquivoInput.files[0];
+    if ( arquivo && arquivo.type !== 'application/pdf') {
+        alert("Porfavor, selecione um arquivo PDF. ")
+    }
+});
+
 
